bc124caf0e363ab9da5aee0647c8ab5fedd9a990
Gaurav Patil <72014112+gauravpatil2468@users.noreply.github.com>
2024-06-22 14:54:43 +0530

    change theme

--- client/src/scenes/navbar/navbar.jsx
+++ client/src/scenes/navbar/navbar.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
             },
           }}
         >
-          Sociopedia
+          linkLounge
         </Typography>
         {isNonMobileScreens && (
           <FlexBetween
